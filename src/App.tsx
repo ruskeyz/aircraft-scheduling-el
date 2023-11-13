434409e5e6305cef136c065993f018ceba7271a0
Ilya Playrcart <ilya@playrcart.com>
2023-11-13 17:36:58 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SchedulingView from "./views/Scheduling/SchedulingView";
+
+function App() {
+  return (
+    <>
+      <SchedulingView />
+    </>
+  );
+}
+
+export default App;
---
+++ src/views/Scheduling/SchedulingView.tsx
@@ -0,0 +1,7 @@
+export default function SchedulingView() {
+  return (
+    <>
+      <div>SchedulingView</div>
+    </>
+  );
+}
